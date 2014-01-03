77a943cf981de853349e89d7c41d841b55cc6a3d
Maxim Pechnikov <parallel588@gmail.com>
2014-01-03 17:55:41 +0300

    fixed supplier class

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,8 +1,8 @@
 Spree.user_class.class_eval do
 
-  belongs_to :supplier
+  belongs_to :supplier, class_name: 'Spree::Supplier'
 
-  has_many :variants, through: :supplier
+  has_many :variants, through: :supplier, class_name: 'Spree::Variant'
 
   def supplier?
     supplier.present?
